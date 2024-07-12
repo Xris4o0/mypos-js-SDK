fd6e03dbf167d4f2c10161d748ee5811a2314460
Xris4o0 <55448896+Xris4o0@users.noreply.github.com>
2024-07-12 14:07:47 +0300

    Update authorization-reverse.js

--- resources/checkout/authorization-reverse.js
+++ resources/checkout/authorization-reverse.js
@@ -10,14 +10,7 @@ class CheckoutAuthorizationReverseRequest extends CheckoutApiRequest {
         let version = utils.safeVal(params.version, utils.safeVal(mypos.config.checkout.version, '1.4'));
         let sid = utils.safeVal(params.sid, mypos.config.checkout.sid);
         let walletNumber = utils.safeVal(params.sid, mypos.config.checkout.clientNumber);
-        let currency = utils.safeVal(params.currency, mypos.config.checkout.currency);
         let orderId = utils.safeVal(params.orderId, uuidv4());
-        let okUrl = utils.safeVal(params.okUrl, mypos.config.checkout.okUrl);
-        let cancelUrl = utils.safeVal(params.cancelUrl, mypos.config.checkout.cancelUrl);
-        let notifyUrl = utils.safeVal(params.notifyUrl, mypos.config.checkout.notifyUrl);
-        let cardToken = utils.safeVal(params.cardToken, utils.safeVal(mypos.config.checkout.cardToken, 0));
-        let paymentMethod = utils.safeVal(params.paymentMethod, utils.safeVal(mypos.config.checkout.paymentMethod, 1));
-        let paymentParametersRequired = utils.safeVal(params.paymentParametersRequired, utils.safeVal(mypos.config.checkout.paymentParametersRequired, 1));
 
         const reverseParams = {
             IPCmethod: 'IPCAuthorizationReverse',
@@ -27,7 +20,7 @@ class CheckoutAuthorizationReverseRequest extends CheckoutApiRequest {
             SID: sid,
             WalletNumber: walletNumber,
             KeyIndex: mypos.config.checkout.keyIndex,
-            OutputFormat: XML, // XML or JSON
+            // OutputFormat: XML, // XML or JSON
         };
 
         super(mypos, reverseParams);
