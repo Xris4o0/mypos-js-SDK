374472909f8d24607e9cbd495a989c04d248fcdf
Xris4o0 <55448896+Xris4o0@users.noreply.github.com>
2024-09-27 13:08:02 +0300

    Update purchase-by-icard.js

--- resources/checkout/purchase-by-icard.js
+++ resources/checkout/purchase-by-icard.js
@@ -4,7 +4,7 @@ const uuidv4 = require('uuid/v4');
 const utils = require('../../utils/common');
 const CheckoutApiRequest = require('../abstract/checkout-api-request');
 
-class CheckoutIAPurchaseRequest extends CheckoutApiRequest {
+class CheckoutIPCPurchaseByIcardRequest extends CheckoutApiRequest {
     constructor(mypos, params) {
         let language = utils.safeVal(params.lang, utils.safeVal(mypos.config.checkout.lang, 'EN'));
         let version = utils.safeVal(params.version, utils.safeVal(mypos.config.checkout.version, '1.4'));
@@ -15,33 +15,22 @@ class CheckoutIAPurchaseRequest extends CheckoutApiRequest {
         let okUrl = utils.safeVal(params.okUrl, mypos.config.checkout.okUrl);
         let cancelUrl = utils.safeVal(params.cancelUrl, mypos.config.checkout.cancelUrl);
         let notifyUrl = utils.safeVal(params.notifyUrl, mypos.config.checkout.notifyUrl);
-        let cardToken = utils.safeVal(params.cardToken, utils.safeVal(mypos.config.checkout.cardToken, 0));
-        let paymentMethod = utils.safeVal(params.paymentMethod, utils.safeVal(mypos.config.checkout.paymentMethod, 1));
-        let paymentParametersRequired = utils.safeVal(params.paymentParametersRequired, utils.safeVal(mypos.config.checkout.paymentParametersRequired, 1));
-
+        
         const purchaseParams = {
             IPCmethod: 'IPCPurchaseByIcard',
             IPCVersion: version,
             IPCLanguage: language,
-            SID: sid,
-            WalletNumber: walletNumber,
             Amount: params.amount,
             Currency: currency,
             OrderID: orderId,
+            SID: sid,
+            WalletNumber: walletNumber,
+            KeyIndex: mypos.config.checkout.keyIndex,
             URL_OK: okUrl,
             URL_Cancel: cancelUrl,
             URL_Notify: notifyUrl,
-            CardToken: cardToken,
-            KeyIndex: mypos.config.checkout.keyIndex,
-            PaymentParametersRequired: paymentParametersRequired,
-            PaymentMethod: paymentMethod,
             customeremail: params.customer.email,
-            customerfirstnames: params.customer.firstNames,
-            customerfamilyname: params.customer.familyName,
             customerphone: params.customer.phone,
-            customercountry: params.customer.country,
-            customercity: params.customer.city,
-            customerzipcode: params.customer.zipCode,
             customeraddress: params.customer.address,
             Note: params.note,
             CartItems: params.cartItems.length
@@ -60,4 +49,4 @@ class CheckoutIAPurchaseRequest extends CheckoutApiRequest {
     }
 }
 
-module.exports = CheckoutIAPurchaseRequest;
+module.exports = CheckoutIPCPurchaseByIcardRequest;
