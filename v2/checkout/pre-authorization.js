b8f89c5a848ca36aee40c0682389504027a25725
Xris4o0 <55448896+Xris4o0@users.noreply.github.com>
2025-11-06 17:55:14 +0200

    Update pre-authorization.js

--- v2/checkout/pre-authorization.js
+++ v2/checkout/pre-authorization.js
@@ -2,17 +2,22 @@
 
 const CheckoutRequest = require('../core/checkout-request');
 const { loadConfig } = require('../config');
-const { buildCartItems, calculateTotal } = require('../utils/cart-builder');
 const { safeVal, generateOrderId } = require('../utils/common');
 
 /**
- * Pre-Authorization Request - Create a pre-authorization with cart
+ * Pre-Authorization Request - Create a pre-authorization
+ * NOTE: PreAuthorization does NOT use cart items, only ItemName and Amount
  */
 class PreAuthorizationRequest extends CheckoutRequest {
   constructor(config, params) {
-    // Build and validate cart items
-    const cartItems = buildCartItems(params.cart, params.discount, params.tip);
-    const amount = params.amount !== undefined ? params.amount : calculateTotal(cartItems);
+    // Validate required fields
+    if (!params.amount && typeof params.amount !== 'number') {
+      throw new Error('amount is required for pre-authorization');
+    }
+    
+    if (!params.itemName && !params.ItemName) {
+      throw new Error('itemName is required for pre-authorization');
+    }
     
     // Map to IPC parameters
     const ipcParams = {
@@ -21,41 +26,17 @@ class PreAuthorizationRequest extends CheckoutRequest {
       IPCLanguage: safeVal(params.lang, config.lang),
       SID: safeVal(params.sid, config.sid),
       WalletNumber: safeVal(params.walletNumber, config.clientNumber),
-      Amount: amount,
+      Amount: params.amount,
       Currency: safeVal(params.currency, config.currency),
       OrderID: safeVal(params.orderId, generateOrderId()),
       URL_OK: safeVal(params.successUrl, config.successUrl),
       URL_Cancel: safeVal(params.cancelUrl, config.cancelUrl),
       URL_Notify: safeVal(params.notifyUrl, config.notifyUrl),
-      CardTokenRequest: safeVal(params.cardTokenRequest, config.cardTokenRequest),
       KeyIndex: safeVal(params.keyIndex, config.keyIndex),
-      PaymentParametersRequired: safeVal(params.paymentParametersRequired, config.paymentParametersRequired),
-      PaymentMethod: safeVal(params.paymentMethod, config.paymentMethod),
-      Note: params.note,
-      CartItems: cartItems.length
+      ItemName: params.itemName || params.ItemName,
+      AccountSettlement: params.accountSettlement || params.AccountSettlement,
+      Note: params.note
     };
-    
-    // Add customer details if provided
-    if (params.customer) {
-      ipcParams.CustomerEmail = params.customer.email;
-      ipcParams.CustomerFirstNames = params.customer.firstNames;
-      ipcParams.CustomerFamilyName = params.customer.familyName;
-      ipcParams.CustomerPhone = params.customer.phone;
-      ipcParams.CustomerCountry = params.customer.country;
-      ipcParams.CustomerCity = params.customer.city;
-      ipcParams.CustomerZIPCode = params.customer.zipCode;
-      ipcParams.CustomerAddress = params.customer.address;
-    }
-    
-    // Add cart items
-    cartItems.forEach((item, index) => {
-      const num = index + 1;
-      ipcParams[`Article_${num}`] = item.name;
-      ipcParams[`Quantity_${num}`] = item.quantity;
-      ipcParams[`Price_${num}`] = item.price;
-      ipcParams[`Currency_${num}`] = ipcParams.Currency;
-      ipcParams[`Amount_${num}`] = item.price * item.quantity;
-    });
     
     super(config, ipcParams);
   }
@@ -64,9 +45,10 @@ class PreAuthorizationRequest extends CheckoutRequest {
 /**
  * Create a pre-authorization
  * @param {Object} params - Parameters
- * @param {Array} params.cart - Cart items
- * @param {Object} params.customer - Customer info
+ * @param {number} params.amount - Amount to pre-authorize (REQUIRED)
+ * @param {string} params.itemName - Description of item (REQUIRED)
  * @param {string} params.currency - Currency code
+ * @param {string} params.accountSettlement - Optional account settlement
  * @param {string} params.note - Optional note
  * @returns {Promise<Object>} {redirectUrl, rawResponse}
  */
